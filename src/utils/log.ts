049568f6a554cd058530c5dd783dacf63d4e2fa2
Confettis <80667818+Confettis@users.noreply.github.com>
2021-10-18 15:08:15 +0800

    feat: 封装 log 日志功能

---
+++ src/utils/log.ts
@@ -0,0 +1,46 @@
+import Taro from "@tarojs/taro";
+
+const log = Taro.getRealtimeLogManager();
+const isDev = process.env.NODE_ENV === "development";
+
+export default {
+  info(...args: any[]) {
+    if (isDev) {
+      return console.info(...args);
+    }
+    if (!log) {
+      return;
+    }
+    log.info(...args);
+  },
+  warn(...args: any[]) {
+    if (isDev) {
+      return console.warn(...args);
+    }
+    if (!log) {
+      return;
+    }
+    log.warn(...args);
+  },
+  error(...args: any[]) {
+    if (isDev) {
+      return console.error(...args);
+    }
+    if (!log) {
+      return;
+    }
+    log.error(...args);
+  },
+  setFilterMsg(msg: string) {
+    if (!log || !log.setFilterMsg || isDev) {
+      return;
+    }
+    log.setFilterMsg(msg);
+  },
+  addFilterMsg(msg: string) {
+    if (!log || !log.addFilterMsg || isDev) {
+      return;
+    }
+    log.addFilterMsg(msg);
+  }
+};
