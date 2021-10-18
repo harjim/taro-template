c37ba851f95cb2fe6060353429b437181e0aaf20
Confettis <80667818+Confettis@users.noreply.github.com>
2021-10-18 15:26:27 +0800

    style: 调整 eslint 规则

--- src/app.tsx
+++ src/app.tsx
@@ -1,47 +1,45 @@
-import { Component } from "react";
-import { Provider } from "react-redux";
+import { Component } from "react"
+import { Provider } from "react-redux"
 
-import store from "./store/index";
-import log from "./utils/log";
-import "./app.scss";
+import store from "./store/index"
+import log from "./utils/log"
+import "./app.scss"
 
 class App extends Component {
   componentDidMount() {
-    const updateManager = Taro.getUpdateManager();
+    const updateManager = Taro.getUpdateManager()
 
     updateManager.onCheckForUpdate((res) => {
-      console.log(res);
-    });
+      console.log(res)
+    })
 
     updateManager.onUpdateReady(() => {
       Taro.showModal({
-        title: "更新提示",
-        content: "新版本准备就绪，是否重启应用？",
+        title: '更新提示',
+        content: '新版本准备就绪，是否重启应用？',
         success: (res) => {
           if (res.confirm) {
-            updateManager.applyUpdate();
+            updateManager.applyUpdate()
           }
-        }
-      });
-    });
+        },
+      })
+    })
     updateManager.onUpdateFailed((res) => {
-      console.log(res);
-    });
+      console.log(res)
+    })
   }
 
-  componentDidShow() {
-  }
+  componentDidShow() {}
 
-  componentDidHide() {
-  }
+  componentDidHide() {}
 
   componentDidCatchError(e: string) {
-    log.error(e);
+    log.error(e)
   }
 
   // this.props.children 是将要会渲染的页面
   render() {
-    return <Provider store={store}>{this.props.children}</Provider>;
+    return <Provider store={store}>{this.props.children}</Provider>
   }
 }
 
